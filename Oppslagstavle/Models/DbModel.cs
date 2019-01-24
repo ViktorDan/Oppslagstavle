0a97c137a3f2b21bf68c4ec6e56bb6097a5ca532
Kristian Ludvig Grønvold <s315691@oslomet.no>
2019-01-24 13:11:20 +0100

    la til DB branch

---
+++ Oppslagstavle/Models/DbModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Oppslagstavle.Models
+{
+    public class DbModel
+    {
+        public class Bygg
+        {
+
+        }
+    }
+}
