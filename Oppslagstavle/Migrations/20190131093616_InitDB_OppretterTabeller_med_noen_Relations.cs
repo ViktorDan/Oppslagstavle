c10737bcd2f4bce25a7f6093105c9c1a94d8d9ad
Viktor Danielsen <viktordan9019@gmail.com>
2019-01-31 10:44:55 +0100

    opprettelse av tabeller og noen relations

---
+++ Oppslagstavle/Migrations/20190131093616_InitDB_OppretterTabeller_med_noen_Relations.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Oppslagstavle.Migrations
+{
+    public partial class InitDB_OppretterTabeller_med_noen_Relations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DB_Beboere",
+                columns: table => new
+                {
+                    BeboerId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DB_Beboere", x => x.BeboerId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DB_Borettslag",
+                columns: table => new
+                {
+                    BorettslagId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Navn = table.Column<string>(nullable: true),
+                    Opprettet = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DB_Borettslag", x => x.BorettslagId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DB_Bygg",
+                columns: table => new
+                {
+                    ByggId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ByggNr = table.Column<int>(nullable: false),
+                    ByggType = table.Column<string>(nullable: true),
+                    ByggNavn = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DB_Bygg", x => x.ByggId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DB_Enheter",
+                columns: table => new
+                {
+                    EnhetId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    EnhetsNr = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DB_Enheter", x => x.EnhetId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DB_Oppslag",
+                columns: table => new
+                {
+                    OppslagId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Tittel = table.Column<string>(nullable: true),
+                    Tekst = table.Column<string>(nullable: true),
+                    Bilde = table.Column<string>(nullable: true),
+                    StartDato = table.Column<DateTime>(nullable: false),
+                    SluttDato = table.Column<DateTime>(nullable: false),
+                    StartTid = table.Column<string>(nullable: true),
+                    SluttTid = table.Column<string>(nullable: true),
+                    Deltakelse = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DB_Oppslag", x => x.OppslagId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DB_Personer",
+                columns: table => new
+                {
+                    PersonId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Fornavn = table.Column<string>(nullable: true),
+                    Etternavn = table.Column<string>(nullable: true),
+                    Fulltnavn = table.Column<string>(nullable: true),
+                    Epost = table.Column<string>(nullable: true),
+                    Lowercase_Epost = table.Column<string>(nullable: true),
+                    Tlf = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DB_Personer", x => x.PersonId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DB_Styremedlemmer",
+                columns: table => new
+                {
+                    StyremedlemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Styreleder = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DB_Styremedlemmer", x => x.StyremedlemId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DB_OppslagIBygg",
+                columns: table => new
+                {
+                    ByggId = table.Column<int>(nullable: false),
+                    OppslagId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DB_OppslagIBygg", x => new { x.ByggId, x.OppslagId });
+                    table.ForeignKey(
+                        name: "FK_DB_OppslagIBygg_DB_Bygg_ByggId",
+                        column: x => x.ByggId,
+                        principalTable: "DB_Bygg",
+                        principalColumn: "ByggId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DB_OppslagIBygg_DB_Oppslag_OppslagId",
+                        column: x => x.OppslagId,
+                        principalTable: "DB_Oppslag",
+                        principalColumn: "OppslagId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DB_OppslagIBygg_OppslagId",
+                table: "DB_OppslagIBygg",
+                column: "OppslagId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DB_Beboere");
+
+            migrationBuilder.DropTable(
+                name: "DB_Borettslag");
+
+            migrationBuilder.DropTable(
+                name: "DB_Enheter");
+
+            migrationBuilder.DropTable(
+                name: "DB_OppslagIBygg");
+
+            migrationBuilder.DropTable(
+                name: "DB_Personer");
+
+            migrationBuilder.DropTable(
+                name: "DB_Styremedlemmer");
+
+            migrationBuilder.DropTable(
+                name: "DB_Bygg");
+
+            migrationBuilder.DropTable(
+                name: "DB_Oppslag");
+        }
+    }
+}
--- Oppslagstavle/Models/DbModel.cs
+++ Oppslagstavle/Models/DbModel.cs
@@ -15,6 +15,8 @@ namespace Oppslagstavle.Models
             : base(options)
         { }
 
+        
+
         public DbSet<Borettslag> DB_Borettslag { get; set; }
         public DbSet<Bygg> DB_Bygg { get; set; }
         public DbSet<Enhet> DB_Enheter { get; set; }
@@ -24,44 +26,72 @@ namespace Oppslagstavle.Models
         public DbSet<Oppslag> DB_Oppslag { get; set; }
         public DbSet<OppslagIBygg> DB_OppslagIBygg { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<OppslagIBygg>()
+                .HasKey(k => new { k.ByggId, k.OppslagId });
+
+            modelBuilder.Entity<OppslagIBygg>()
+                .HasOne(p => p.Bygg)
+                .WithMany(b => b.OppslagIBygg)
+                .HasForeignKey(p => p.ByggId);
+            modelBuilder.Entity<OppslagIBygg>()
+                 .HasOne(p => p.Oppslag)
+                 .WithMany(b => b.OppslagIBygg)
+                 .HasForeignKey(p => p.OppslagId);
+
+            modelBuilder.Entity<Bygg>()
+                .HasKey(k => k.ByggId);
+
+            modelBuilder.Entity<Enhet>()
+                .HasKey(k => k.EnhetId);
+
+            modelBuilder.Entity<Person>()
+                .HasKey(k => k.PersonId);
+
+            modelBuilder.Entity<Beboer>()
+                .HasKey(k => k.BeboerId);
+
+            modelBuilder.Entity<Styremedlem>()
+                .HasKey(k => k.StyremedlemId);
+
+            modelBuilder.Entity<Oppslag>()
+                .HasKey(k => k.OppslagId);
+        }
 
         public class Borettslag
         {
-            [Key]
             public int BorettslagId { get; set; }
             public string Navn { get; set; }
             public DateTime Opprettet { get; set; }
             // Fremmednøkler
-            public List<Bygg> Bygg { get; set; } 
-            public List<Styremedlem> Styremedlemmer { get; set; }
+            //public List<Bygg> Bygg { get; set; } 
+            //public List<Styremedlem> Styremedlemmer { get; set; }
         }
         public class Bygg
         {
-            [Key]
             public int ByggId { get; set; }
             public int ByggNr { get; set; }
             public string ByggType { get; set; }
             public string ByggNavn { get; set; }
             // Fremmednøkler
-            public int BorettslagId { get; set; }
-            public Borettslag Borettslag { get; set; }
-            public List<Enhet> Enheter { get; set; }
+            //public int BorettslagId { get; set; }
+            //public Borettslag Borettslag { get; set; }
+            //public List<Enhet> Enheter { get; set; }
             public List<OppslagIBygg> OppslagIBygg { get; set; } // ----------------> Peker til OppslagIBygg
 
         }
         public class Enhet
         {
-            [Key]
             public int EnhetId { get; set; }
+            public int EnhetsNr { get; set; }
             // Fremmednøkler
-            public int ByggId { get; set; }
-            public Bygg Bygg { get; set; }
-            public List<Beboer> Beboere { get; set; }
+            //public int ByggId { get; set; }
+            //public Bygg Bygg { get; set; }
+            //public List<Beboer> Beboere { get; set; }
         }
-       
         public class Person
         {
-            [Key]
             public int PersonId { get; set; }
             public string Fornavn { get; set; }
             public string Etternavn { get; set; }
@@ -69,23 +99,31 @@ namespace Oppslagstavle.Models
             public string Epost { get; set; }
             public string Lowercase_Epost { get; set; }
             public string Tlf { get; set; }
+            //public int BeboerId { get; set; }
+            //public Beboer Beboer { get; set; }
+            //public int StyremedlemId { get; set; }
+            //public Styremedlem Styremedlem { get; set; }
         }
-        public class Beboer : Person
+        // Må prøve uten inheritance da det skaper cascade delete cycles.
+        public class Beboer
         {
-            public int EnhetId { get; set; }
-            public Enhet Enhet { get; set; }
+            public int BeboerId { get; set; }
+            //public List<Person> Personer { get; set; }
+            //public int EnhetId { get; set; }
+            //public Enhet Enhet { get; set; }
         }
-        public class Styremedlem : Person
+        public class Styremedlem
         {
+            public int StyremedlemId { get; set; }
             public bool Styreleder { get; set; }
             // Fremmednøkler
-            public int BorettslagId { get; set; }
-            public virtual Borettslag Borettslag { get; set; }
-            public List<Oppslag> Oppslag { get; set; }
+            //public List<Person> Personer { get; set; }
+            //public int BorettslagId { get; set; }
+            //public Borettslag Borettslag { get; set; }
+            //public List<Oppslag> Oppslag { get; set; }
         }
         public class Oppslag
         {
-            [Key]
             public int OppslagId { get; set; }
             public string Tittel { get; set; }
             public string Tekst { get; set; }
@@ -96,13 +134,12 @@ namespace Oppslagstavle.Models
             public string SluttTid { get; set; }
             public bool Deltakelse { get; set; }
             // Fremmednøkler
-            public int PersonId { get; set; } // ----------------> Peker til Styremedlem
-            public Styremedlem Styremedlem { get; set; }
+            //public int PersonId { get; set; } // ----------------> Peker til Styremedlem
+            //public Styremedlem Styremedlem { get; set; }
             public List<OppslagIBygg> OppslagIBygg { get; set; } // ----------------> Peker til OppslagIBygg
         }
         public class OppslagIBygg
         {
-            public int OppslagIByggId { get; set; }
             // Mellom-entitet i mange-til-mange forhold Oppslag og Bygg
             public int ByggId { get; set; }
             public Bygg Bygg { get; set; }
